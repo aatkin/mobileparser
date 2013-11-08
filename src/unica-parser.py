b58deb3a4cf5a0322026536e48c9d012d03e4e4e
Wille2 <villeahti@hotmail.com>
2013-11-08 23:50:17 +0200

    little prettifying

--- src/unica-parser.py
+++ src/unica-parser.py
@@ -2,32 +2,33 @@ from bs4 import BeautifulSoup as BS
 import urllib2 as url
 import re, json, os
 
+print "loading page..."
 page = url.urlopen("http://www.unica.fi/fi/ravintolat/mikro/").read()
 
 soup = BS(page, from_encoding='utf-8')
-print soup.original_encoding
 
-
+#contains the menu
 menu_list = soup.select(".menu-list")[0]
 
 restaurant_foods = []
 restaurant = {"name" : 'mikro', "days" : restaurant_foods}
 
+#every day is inside accord
 week_days = menu_list.select(".accord")
 for day in week_days:
-	day_name = day.h4.get_text()
-	day_number = day.h4.get("data-dayofweek")
-	day_lunches = map(lambda x: x.get_text().encode('utf-8', 'ignore'), day.table.select(".lunch")) 
-        day_prices = map(lambda y: re.findall(r'\d\,\d\d', y.get_text().encode('ascii','ignore')), day.table.select("[class~=price]"))
-	
-	
-        lunch_price = dict(zip(day_lunches, day_prices))
-	restaurant_foods.append({"day": day_name, "foods" : lunch_price})
+    day_name = day.h4.get_text()
+    day_number = day.h4.get("data-dayofweek")
+    day_lunches = map(lambda x: x.get_text().encode('utf-8', 'ignore'), day.table.select(".lunch")) 
+    day_prices = map(lambda y: re.findall(r'\d\,\d\d', y.get_text().encode('ascii','ignore')), day.table.select("[class~=price]"))
+    lunch_price = dict(zip(day_lunches, day_prices))
+    restaurant_foods.append({"day": day_name, "foods" : lunch_price})
 
+print "creating json format..."
 json_format = json.dumps(restaurant, sort_keys=True, indent=4, separators=(',', ': '))
 
 directory = '../output/'
 if not os.path.exists(directory):
     os.makedirs(directory)
 
+print "writing to file..."
 open(directory + "mikro.json","w+").write(json_format)
