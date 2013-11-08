8aab6a830c7792a678a7ccd563b4617c9488f7bb
Wille2 <villeahti@hotmail.com>
2013-11-08 23:21:38 +0200

    one restaurants weekly foods in json format

--- src/unica-parser.py
+++ src/unica-parser.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup as BS
 import urllib2 as url
 import re
+import json
 
 page = url.urlopen("http://www.unica.fi/fi/ravintolat/mikro/").read()
 
@@ -11,13 +12,15 @@ print soup.original_encoding
 menu_list = soup.select(".menu-list")[0]
 
 week_days = menu_list.select(".accord")
+restaurant = []
 for day in week_days:
 	day_name = day.h4.get_text()
 	day_number = day.h4.get("data-dayofweek")
 	day_lunches = map(lambda x: x.get_text().encode('utf-8', 'ignore'), day.table.select(".lunch")) 
         day_prices = map(lambda y: re.findall(r'\d\,\d\d', y.get_text().encode('ascii','ignore')), day.table.select("[class~=price]"))
 	
-	print day_number, day_name
-        print dict(zip(day_lunches, day_prices))
-	print ' / '.join(day_prices[0])
+	
+        lunch_price = dict(zip(day_lunches, day_prices))
+	restaurant.append({"day": day_name, "foods" : lunch_price})
 
+print json.dumps(restaurant, sort_keys=True, indent=4, separators=(',', ': '))
