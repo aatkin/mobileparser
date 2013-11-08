d566b53e637cf1a180e75ac47b38e213d489372e
Wiltzu <ville.ahti2@gmail.com>
2013-11-08 20:07:44 +0200

    menu list parsed still work to do

--- src/unica-parser.py
+++ src/unica-parser.py
@@ -2,5 +2,12 @@ from bs4 import BeautifulSoup as BS
 import urllib2 as url
 
 page = url.urlopen("http://www.unica.fi/fi/ravintolat/mikro/", data="UTF-8").read()
-print page
+
+soup = BS(page)
+
+menu_list = soup.select(".menu-list")[0]
+week_day = menu_list.select("h4[data-dayofweek='0'] + table")
+foods = week_day
+print foods
+print soup.select("#side-navi")[0].find_all("li")[0].get_text()
 
