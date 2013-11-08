671e187e7b549553701d710f00bea6ac4999a3a9
Wille2 <villeahti@hotmail.com>
2013-11-08 23:34:06 +0200

    now quite good restaurant json is generated, but there are still things to do

--- src/unica-parser.py
+++ src/unica-parser.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup as BS
 import urllib2 as url
-import re
-import json
+import re, json, os
 
 page = url.urlopen("http://www.unica.fi/fi/ravintolat/mikro/").read()
 
@@ -11,8 +10,10 @@ print soup.original_encoding
 
 menu_list = soup.select(".menu-list")[0]
 
+restaurant_foods = []
+restaurant = {"name" : 'mikro', "days" : restaurant_foods}
+
 week_days = menu_list.select(".accord")
-restaurant = []
 for day in week_days:
 	day_name = day.h4.get_text()
 	day_number = day.h4.get("data-dayofweek")
@@ -21,6 +22,12 @@ for day in week_days:
 	
 	
         lunch_price = dict(zip(day_lunches, day_prices))
-	restaurant.append({"day": day_name, "foods" : lunch_price})
+	restaurant_foods.append({"day": day_name, "foods" : lunch_price})
 
-print json.dumps(restaurant, sort_keys=True, indent=4, separators=(',', ': '))
+json_format = json.dumps(restaurant, sort_keys=True, indent=4, separators=(',', ': '))
+
+directory = '../output/'
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+open(directory + "mikro.json","w+").write(json_format)
