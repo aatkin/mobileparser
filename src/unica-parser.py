bab51560283a031bf4bee63db36196d797bb75cb
Wille2 <villeahti@hotmail.com>
2013-11-14 23:08:03 +0200

    now foods are saved by day also

--- src/unica-parser.py
+++ src/unica-parser.py
@@ -3,42 +3,100 @@ import urllib2 as url
 import re, json, os
 import restaurant_urls as rest_urls
 
-restaurants = []
 
-for link in rest_urls.UNICA_URLS:
-	print "loading page..."
-	page = url.urlopen(link).read()
+def parse():
+	restaurants = []
 
-	soup = BS(page, from_encoding='utf-8')
+	for link in rest_urls.UNICA_URLS:
+		print "loading page..."
+		page = url.urlopen(link).read()
 
-	#contains the menu
-	menu_list = soup.select(".menu-list")[0]
+		soup = BS(page, from_encoding='utf-8')
 
-	restaurant_foods = []
-	restaurant = {"name" : soup.select(".head")[0].get_text().strip(), "days" : restaurant_foods}
+		#contains the menu
+		menu_list = soup.select(".menu-list")[0]
 
-	#every day is inside accord
-	week_days = menu_list.select(".accord")
-	for day in week_days:
-	    day_name = day.h4.get_text()
-	    day_number = day.h4.get("data-dayofweek")
-	    day_lunches = map(lambda x: x.get_text().encode('utf-8', 'ignore'), day.table.select(".lunch")) 
-	    day_prices = map(lambda y: re.findall(r'\d\,\d\d', y.get_text().encode('ascii','ignore')), day.table.select("[class~=price]"))
-	    lunch_price = dict(zip(day_lunches, day_prices))
-	    restaurant_foods.append({"day": day_name, "foods" : lunch_price})
+		restaurant_foods = []
+		restaurant = {"name" : soup.select(".head")[0].get_text().strip(), "days" : restaurant_foods}
 
-	restaurants.append(restaurant)
+		#every day is inside accord
+		week_days = menu_list.select(".accord")
+		for day in week_days:
+		    day_name = day.h4.get_text()
+		    day_number = int(day.h4.get("data-dayofweek"))
+		    day_lunches = map(lambda x: x.get_text().encode('utf-8', 'ignore'), day.table.select(".lunch")) 
+		    day_prices = map(lambda y: re.findall(r'\d\,\d\d', y.get_text().encode('ascii','ignore')), day.table.select("[class~=price]"))
+		    lunch_price = dict(zip(day_lunches, day_prices))
+		    restaurant_foods.append({"day": day_number, "foods" : lunch_price})
 
-	print "creating json format..."
-	json_format = json.dumps(restaurant, sort_keys=True, indent=4, separators=(',', ': '))
+		restaurants.append(restaurant)
 
-	directory = '../output/' + restaurant['name']
-	if not os.path.exists(directory):
-	    os.makedirs(directory)
+		write_output_file(get_json(restaurant), restaurant_name=restaurant['name'], file_type="json")
 
-	print "writing to file..."
-	open(directory + restaurant['name'] + ".json","w+").write(json_format)
+	#daily foods in one file for now
+	week_day_all = []
+	for rest in restaurants:
+		rest_name = rest['name']
+		for day in rest['days']:
+			day_number = day['day']
+			day_foods = day['foods']
+			
+			if day_number not in range(len(week_day_all)):
+				week_day_all.append({'day': day_number, 'restaurants': []})
+			
+			week_day_all[day_number]['restaurants'].append({'restaurant': rest_name, 'foods' : day_foods})
 
-for rest in restaurants:
-	print rest['days'][0]['day']
-	print rest['days'][0]['foods']
+	write_output_file(get_json(week_day_all), restaurant_name="unica", file_type="json")
+			
+
+
+def get_json(data):
+    """
+	Creates and returns a json from data.
+	Parameters
+	-----------
+    data: Object
+    Returns
+    --------
+    data: Object
+    data in json format
+	"""
+    print "creating json format..."
+    return json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
+
+def write_output_file(data, restaurant_name="", file_type="json"):
+    """
+    Writes data to file that is placed in output directory.
+    Parameters
+    -----------
+    data: Object
+    restaurant_name: str
+    file_type: str
+    """
+    restaurant_name = format_name(restaurant_name)
+    directory = '../output/%s' % restaurant_name
+    #make the directory above if it doesn't exist
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    filename = "%(dir)s/%(filename)s.%(filetype)s" % {'dir' : directory,'filename' : restaurant_name, 'filetype' : file_type}
+    print "writing to file %s..." % filename
+    #Write with w+ writes
+    open(filename,"w+").write(data)
+
+def format_name(name):
+    """
+    Formats string to filename compatible format.
+    Parameters
+    -----------
+    name: str
+    Returns
+    --------
+    name: str
+    Formatted name    
+    """
+    return name.strip().replace(" ", "_")
+
+if __name__ == '__main__':
+    parse()
+
