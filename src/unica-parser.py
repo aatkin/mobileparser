3a4dd7ab95b0aa3b3343d913a83357687c6852eb
Wille2 <villeahti@hotmail.com>
2013-11-21 18:49:05 +0200

    refactored unica-parser

--- src/unica-parser.py
+++ src/unica-parser.py
@@ -1,68 +1,101 @@
 from bs4 import BeautifulSoup as BS
 import urllib2 as url
-import re, json, os
+import re
+import json
+import os
+import sys
+import datetime
+import logging
 import restaurant_urls as rest_urls
 
+logging.basicConfig(level=logging.INFO)
+LOG = logging.getLogger(' unica-parser ')
 
-def parse():
-	restaurants = []
 
-	for link in rest_urls.UNICA_URLS:
-		print "loading page..."
-		page = url.urlopen(link).read()
+def load_page(link):
+    page = None
 
-		soup = BS(page, from_encoding='utf-8')
+    try:
+        LOG.info(" Loading page... " + link)
+        page = url.urlopen(link).read()
 
-		#contains the menu
-		menu_list = soup.select(".menu-list")[0]
+    except url.URLError, e:
+        LOG.exception("Connection failed to UNICA")
+        return
 
-		restaurant_foods = []
-		restaurant = {"name" : soup.select(".head")[0].get_text().strip(), "days" : restaurant_foods}
+    return BS(page, from_encoding='utf-8')
 
-		#every day is inside accord
-		week_days = menu_list.select(".accord")
-		for day in week_days:
-		    day_name = day.h4.get_text()
-		    day_number = int(day.h4.get("data-dayofweek"))
-		    day_lunches = map(lambda x: x.get_text().encode('utf-8', 'ignore'), day.table.select(".lunch")) 
-		    day_prices = map(lambda y: re.findall(r'\d\,\d\d', y.get_text().encode('ascii','ignore')), day.table.select("[class~=price]"))
-		    lunch_price = dict(zip(day_lunches, day_prices))
-		    restaurant_foods.append({"day": day_number, "foods" : lunch_price})
 
-		restaurants.append(restaurant)
+def parse(expected_week_number):
+    """
+    """
+    restaurants = []
 
-		write_output_file(get_json(restaurant), restaurant_name=restaurant['name'], file_type="json")
+    for link in rest_urls.UNICA_URLS:
+        soup = load_page(link)
 
-	#daily foods in one file for now
-	week_day_all = []
-	for rest in restaurants:
-		rest_name = rest['name']
-		for day in rest['days']:
-			day_number = day['day']
-			day_foods = day['foods']
-			
-			if day_number not in range(len(week_day_all)):
-				week_day_all.append({'day': day_number, 'restaurants': []})
-			
-			week_day_all[day_number]['restaurants'].append({'restaurant': rest_name, 'foods' : day_foods})
+        week_number = int(
+            re.findall(r'\d\d', soup.select("div.pad > h3.head2")[0].get_text().encode('ascii', 'ignore'))[0])
+        if(week_number != expected_week_number):
+            LOG.error(' Expected week number was ' + str(expected_week_number) + ' but actual was ' + str(week_number))
+            return
+        # contains the menu
+        menu_list = soup.select(".menu-list")[0]
 
-	write_output_file(get_json(week_day_all), restaurant_name="unica", file_type="json")
-			
+        restaurant_foods = []
+        restaurant = {"name": soup.select(".head")[
+                      0].get_text().strip(), "days": restaurant_foods}
+
+        # every day is inside accord
+        week_days = menu_list.select(".accord")
+        for day in week_days:
+            day_name = day.h4.get_text()
+            day_number = int(day.h4.get("data-dayofweek"))
+            day_lunches = map(lambda x: x.get_text().encode(
+                'utf-8', 'ignore'), day.table.select(".lunch"))
+            day_prices = map(lambda y: re.findall(
+                r'\d\,\d\d', y.get_text().encode('ascii', 'ignore')), day.table.select("[class~=price]"))
+            lunch_price = dict(zip(day_lunches, day_prices))
+            restaurant_foods.append(
+                {"day": day_number, "foods": lunch_price})
+
+        restaurants.append(restaurant)
+
+        write_output_file(
+            get_json(restaurant), restaurant_name=restaurant['name'], file_type="json")
+
+        # daily foods in one file for now
+        week_day_all = []
+        for rest in restaurants:
+            rest_name = rest['name']
+            for day in rest['days']:
+                day_number = day['day']
+                day_foods = day['foods']
+
+                if day_number not in range(len(week_day_all)):
+                    week_day_all.append({'day': day_number, 'restaurants': []})
+
+                week_day_all[day_number]['restaurants'].append(
+                    {'restaurant': rest_name, 'foods': day_foods})
+
+        write_output_file(
+            get_json(week_day_all), restaurant_name="unica", file_type="json")
 
 
 def get_json(data):
     """
-	Creates and returns a json from data.
-	Parameters
-	-----------
+        Creates and returns a json from data.
+        Parameters
+        -----------
     data: Object
     Returns
     --------
     data: Object
     data in json format
-	"""
+        """
     print "creating json format..."
     return json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
+
 
 def write_output_file(data, restaurant_name="", file_type="json"):
     """
@@ -75,14 +108,16 @@ def write_output_file(data, restaurant_name="", file_type="json"):
     """
     restaurant_name = format_name(restaurant_name)
     directory = '../output/%s' % restaurant_name
-    #make the directory above if it doesn't exist
+    # make the directory above if it doesn't exist
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    filename = "%(dir)s/%(filename)s.%(filetype)s" % {'dir' : directory,'filename' : restaurant_name, 'filetype' : file_type}
+    filename = "%(dir)s/%(filename)s.%(filetype)s" % {
+        'dir': directory, 'filename': restaurant_name, 'filetype': file_type}
     print "writing to file %s..." % filename
-    #Write with w+ writes
-    open(filename,"w+").write(data)
+    # Write with w+ writes
+    open(filename, "w+").write(data)
+
 
 def format_name(name):
     """
@@ -93,10 +128,10 @@ def format_name(name):
     Returns
     --------
     name: str
-    Formatted name    
+    Formatted name
     """
     return name.strip().replace(" ", "_")
 
 if __name__ == '__main__':
-    parse()
-
+    # parse(datetime.datetime.now().isocalendar()[1])
+    parse(int(sys.argv[1]))
