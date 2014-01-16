38d01b0a96c77c55a09f1112ae79adab508ba57b
Wiltzu <ville.ahti2@gmail.com>
2014-01-16 15:28:41 +0000

    did some initial refactoring to parser, NOT YET COMPLETE

--- mobilefood_parser/unica_parser.py
+++ mobilefood_parser/unica_parser.py
@@ -26,37 +26,37 @@ def load_page(link):
     return page
 
 
-def assureTheWeekNumber(page, expected_week_number):
-    soup = BS(page, from_encoding='utf-8')
+class UnicaParser:
 
-    week_number = int(
-        re.findall(r'\d\d', soup.select("div.pad > h3.head2")[0].get_text().encode('ascii', 'ignore'))[0])
-    if(week_number != expected_week_number):
-        LOG.error(' Expected week number was ' + str(
-            expected_week_number) + ' but actual was ' + str(week_number))
-        return -1
+    def __init__(self, week_number):
+        self.week_number = week_number
 
-
-def parse(expected_week_number):
-    """
-    """
-    restaurants = []
-
-    for link in rest_urls.UNICA_URLS:
-        soup = BS(load_page(link), from_encoding='utf-8')
-
-        week_number = int(
+    def assureTheWeekNumberIsSameAsInThePage(self, soup):
+        current_week_number = int(
             re.findall(r'\d\d', soup.select("div.pad > h3.head2")[0].get_text().encode('ascii', 'ignore'))[0])
-        if(week_number != expected_week_number):
+        if(current_week_number != self.week_number):
             LOG.error(' Expected week number was ' + str(
-                expected_week_number) + ' but actual was ' + str(week_number))
+                self.week_number) + ' but actual was ' + str(self.week_number))
+            return -1
+        else:
+            return 0
+
+    def parse(self, page):
+        """
+        Returns: list of foods by a day
+        """
+        restaurants = []
+
+        soup = BS(page, from_encoding='utf-8')
+
+        if self.assureTheWeekNumberIsSameAsInThePage(soup) == -1:
             return -1
         # contains the menu
         menu_list = soup.select(".menu-list")[0]
-
-        restaurant_foods = []
-        restaurant = {"name": soup.select(".head")[
-                      0].get_text().strip(), "days": restaurant_foods}
+        
+        foodsByADay = []
+        restaurants_foods = {'restaurant_name' : soup.select(".head")[
+            0].get_text().strip(), 'lunches_by_day' : foodsByADay}
 
         # every day is inside accord
         week_days = menu_list.select(".accord")
@@ -66,31 +66,30 @@ def parse(expected_week_number):
                 'utf-8', 'ignore'), day.table.select(".lunch"))
             day_prices = map(lambda y: re.findall(
                 r'\d\,\d\d', y.get_text().encode('ascii', 'ignore')), day.table.select("[class~=price]"))
-            lunch_price = dict(zip(day_lunches, day_prices))
-            restaurant_foods.append(
-                {"day": day_number, "foods": lunch_price})
-
-        restaurants.append(restaurant)
+            lunches_to_prices = dict(zip(day_lunches, day_prices))
+            foodsByADay.append({"day_of_the_week" : day_number, "food": lunches_to_prices})
 
         write_output_file(
-            get_json(restaurant), week_number, restaurant_name=restaurant['name'], file_type="json")
+            get_json(restaurants_foods), self.week_number, restaurant_name=restaurants_foods['restaurant_name'], file_type="json")
 
-    # daily foods in one file for now
-    week_day_all = []
-    for rest in restaurants:
-        rest_name = rest['name']
-        for day in rest['days']:
-            day_number = day['day']
-            day_foods = day['foods']
+        """
+        # daily foods in one file for now
+        week_day_all = []
+        for rest in restaurants:
+            rest_name = rest['name']
+            for day in rest['days']:
+                day_number = day['day']
+                day_foods = day['foods']
 
-            if day_number not in range(len(week_day_all)):
-                week_day_all.append({'day': day_number, 'restaurants': []})
+                if day_number not in range(len(week_day_all)):
+                    week_day_all.append({'day': day_number, 'restaurants': []})
 
-            week_day_all[day_number]['restaurants'].append(
-                {'restaurant': rest_name, 'foods': day_foods})
+                week_day_all[day_number]['restaurants'].append(
+                    {'restaurant': rest_name, 'foods': day_foods})
 
-    write_output_file(
-        get_json(week_day_all), week_number, restaurant_name="unica", file_type="json")
+        write_output_file(
+            get_json(week_day_all), week_number, restaurant_name="unica", file_type="json")
+        """
 
 
 def get_json(data):
@@ -147,4 +146,7 @@ def format_name(name):
 
 if __name__ == '__main__':
     # parse(datetime.datetime.now().isocalendar()[1])
-    sys.exit(parse(int(sys.argv[1])))
+    parser = UnicaParser(int(sys.argv[1]))
+    for link in rest_urls.UNICA_URLS:
+        #sys.exit(parser.parse(load_page(link)))
+        parser.parse(load_page(link))
--- tests/test_unica_parser.py
+++ tests/test_unica_parser.py
@@ -1,14 +1,22 @@
 import unittest
 from datetime import datetime
-from mobilefood_parser import unica_parser
+from mobilefood_parser.unica_parser import *
+
 
 class UnicaParserTests(unittest.TestCase):
 
 	def testThatParseShouldFailIfWeekIsNotCurrent(self):
-		year, week_number, week_day = datetime.now().isocalendar()
-		old_week_number = week_number - 1 
-		self.assertEquals(unica_parser.parse(old_week_number), -1)
+		current_week_number = 49
+		old_week_number = current_week_number - 1
+		parser = UnicaParser(week_number=old_week_number)
+		test_file = open(build_relative_path_to('resources\\Mikro_Unica.htm'))
+		self.assertEquals(-1, parser.parse(test_file))
+
+	def testThatParseFailsWhenDocumentChange(self):
+		pass
+
+def build_relative_path_to(resource):
+	return os.path.join(os.path.dirname(__file__), resource)
 
 if __name__ == '__main__':
     unittest.main()
-
