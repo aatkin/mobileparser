16b0a3aea514b742a95f28e6f5fa55bbcd1209a9
Anssi Kinnunen <aatkin@utu.fi>
2014-05-08 16:21:51 +0300

    added missing dependency

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ config = {
     'author': 'Ville & Anssi',
     'url': 'https://github.com/Wiltzu/mobilefood_parser',
     'version': '0.2.2',
-    'install_requires': ['nose', 'beautifulsoup4', 'lxml'],
+    'install_requires': ['nose', 'beautifulsoup4', 'lxml', 'jsonpickle'],
     'packages': ['mobilefood_parser', 'tests'],
     'scripts': [],
     'name': 'mobilefood_parser'
