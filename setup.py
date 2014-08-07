24ebe8fa693c73830b19027fa5c174a9a8153061
Anssi Kinnunen <aatkin@utu.fi>
2014-08-07 17:52:29 +0300

    updated setup files

--- setup.py
+++ setup.py
@@ -9,30 +9,16 @@ packages = [
 install_requires = [
 ]
 
-tests_requires = [
-]
-
-# config = {
-#     'description': 'Restaurant food parsers for mobilefood project',
-#     'author': 'Ville & Anssi',
-#     'url': 'https://github.com/Wiltzu/mobilefood_parser',
-#     'version': '0.2.2',
-#     'install_requires': ['nose', 'beautifulsoup4', 'lxml', 'jsonpickle'],
-#     'packages': ['mobilefood_parser', 'tests'],
-#     'scripts': [],
-#     'name': 'mobilefood_parser'
-# }
-
 setup(
     name='mobileparser',
     description='Restaurant food list parser for Mobilefood project',
     version=__version__,
     url='https://github.com/aatkin/mobilefood-parser',
     author='Anssi Kinnunen',
-    license='MIT'
+    author='aatkin@utu.fi',
+    license='MIT License'
     packages=packages,
-    include_package_data=True,
-    zip_save=False,
     install_requires=install_requires,
-    tests_requires=tests_requires
+    include_package_data=True,
+    zip_save=False
     )
