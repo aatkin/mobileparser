7371a31db22ec05fe63b16f861caac38fff77229
Anssi Kinnunen <aatkin@utu.fi>
2014-08-15 14:51:09 +0300

    added logging facility

--- mobileparser/main.py
+++ mobileparser/main.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import logging
 from __init__ import __version__
 from sodexo import Sodexo
 from unica import Unica
 
 
 def main(argv):
-    unica_parser = Unica()
-    sodexo_parser = Sodexo()
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(' Mobileparser')
+    unica_parser = Unica(logger)
+    sodexo_parser = Sodexo(logger)
     print "Mobileparser", __version__
     print unica_parser.name, unica_parser.version
     print sodexo_parser.name, sodexo_parser.version
