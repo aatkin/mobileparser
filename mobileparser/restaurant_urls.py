af910836f5e4c2ff4a1091d21021b22f78a8518d
Anssi Kinnunen <aatkin@utu.fi>
2014-08-19 17:50:16 +0300

    extracted urls back to separate file for modularity

---
+++ mobileparser/restaurant_urls.py
@@ -0,0 +1,71 @@
+UNICA_BASE_URL = "http://www.unica.fi"
+UNICA_ASSARI = {
+    "name": "Assarin ullakko",
+    "url_fi": UNICA_BASE_URL + "/fi/ravintolat/assarin-ullakko/",
+    "url_en": UNICA_BASE_URL + "/en/restaurants/assarin-ullakko/",
+    "url_swe": UNICA_BASE_URL + "/se/restauranger/assarin-ullakko/"
+}
+UNICA_BRYGGE = {
+    "name": "Brygge",
+    "url_fi": UNICA_BASE_URL + "/fi/ravintolat/brygge/",
+    "url_en": UNICA_BASE_URL + "/en/restaurants/brygge/",
+    "url_swe": UNICA_BASE_URL + "/se/restauranger/brygge/"
+}
+UNICA_DELICA = {
+    "name": "Delica",
+    "url_fi": UNICA_BASE_URL + "/fi/ravintolat/delica/",
+    "url_en": UNICA_BASE_URL + "/en/restaurants/delica/",
+    "url_swe": UNICA_BASE_URL + "/se/restauranger/delica/"
+}
+UNICA_DELIPHARMA = {
+    "name": "Delipharma",
+    "url_fi": UNICA_BASE_URL + "/fi/ravintolat/deli-pharma/",
+    "url_en": UNICA_BASE_URL + "/en/restaurants/deli-pharma/",
+    "url_swe": UNICA_BASE_URL + "/se/restauranger/deli-pharma/"
+}
+UNICA_DENTAL = {
+    "name": "Dental",
+    "url_fi": UNICA_BASE_URL + "/fi/ravintolat/dental/",
+    "url_en": UNICA_BASE_URL + "/en/restaurants/dental/",
+    "url_swe": UNICA_BASE_URL + "/se/restauranger/dental/"
+}
+UNICA_MACCIAVELLI = {
+    "name": "Macciavelli",
+    "url_fi": UNICA_BASE_URL + "/fi/ravintolat/macciavelli/",
+    "url_en": UNICA_BASE_URL + "/en/restaurants/macciavelli/",
+    "url_swe": UNICA_BASE_URL + "/se/restauranger/macciavelli/"
+}
+UNICA_MIKRO = {
+    "name": "Mikro",
+    "url_fi": UNICA_BASE_URL + "/fi/ravintolat/mikro/",
+    "url_en": UNICA_BASE_URL + "/en/restaurants/mikro/",
+    "url_swe": UNICA_BASE_URL + "/se/restauranger/mikro/"
+}
+UNICA_MYSSY = {
+    "name": "Myssy & Silinteri",
+    "url_fi": UNICA_BASE_URL + "/fi/ravintolat/myssy-silinteri/",
+    "url_en": UNICA_BASE_URL + "/en/restaurants/myssy-silinteri/",
+    "url_swe": UNICA_BASE_URL + "/se/restauranger/myssy-silinteri/"
+}
+UNICA_NUTRITIO = {
+    "name": "Nutritio",
+    "url_fi": UNICA_BASE_URL + "/fi/ravintolat/nutritio/",
+    "url_en": UNICA_BASE_URL + "/en/restaurants/nutritio/",
+    "url_swe": UNICA_BASE_URL + "/se/restauranger/nutritio/"
+}
+UNICA_RUOKAKELLO = {
+    "name": "Ruokakello",
+    "url_fi": UNICA_BASE_URL + "/fi/ravintolat/ruokakello/",
+    "url_en": UNICA_BASE_URL + "/en/restaurants/ruokakello/",
+    "url_swe": UNICA_BASE_URL + "/se/restauranger/ruokakello/"
+}
+UNICA_TOTTISALMI = {
+    "name": "Tottisalmi",
+    "url_fi": UNICA_BASE_URL + "/fi/ravintolat/tottisalmi/",
+    "url_en": UNICA_BASE_URL + "/en/restaurants/tottisalmi/",
+    "url_swe": UNICA_BASE_URL + "/se/restauranger/tottisalmi/"
+}
+UNICA_RESTAURANTS = [UNICA_ASSARI, UNICA_BRYGGE, UNICA_DELICA,
+                     UNICA_DELIPHARMA, UNICA_DENTAL, UNICA_MACCIAVELLI,
+                     UNICA_MIKRO, UNICA_MYSSY, UNICA_NUTRITIO,
+                     UNICA_RUOKAKELLO, UNICA_TOTTISALMI]
